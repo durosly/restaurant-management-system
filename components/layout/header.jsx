f2f41dd5408b2d70c50803f386d6a0dce92204cb
durosly <slyboydon1@gmail.com>
2023-01-08 02:22:28 +0100

    update header routes"

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 function Header() {
 	return (
 		<div className="navbar bg-base-100">
@@ -27,32 +29,13 @@ function Header() {
 						className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
 					>
 						<li>
-							<a>Item 1</a>
+							<Link href="/menu">Menu</Link>
 						</li>
 						<li tabIndex={0}>
-							<a className="justify-between">
-								Parent
-								<svg
-									className="fill-current"
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-								>
-									<path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-								</svg>
-							</a>
-							<ul className="p-2">
-								<li>
-									<a>Submenu 1</a>
-								</li>
-								<li>
-									<a>Submenu 2</a>
-								</li>
-							</ul>
+							<Link href="/about-us">About Us</Link>
 						</li>
 						<li>
-							<a>Item 3</a>
+							<Link href="/faq">FAQ</Link>
 						</li>
 					</ul>
 				</div>
@@ -61,37 +44,24 @@ function Header() {
 			<div className="navbar-center hidden lg:flex">
 				<ul className="menu menu-horizontal px-1">
 					<li>
-						<a>Item 1</a>
+						<Link href="/menu">Menu</Link>
 					</li>
 					<li tabIndex={0}>
-						<a>
-							Parent
-							<svg
-								className="fill-current"
-								xmlns="http://www.w3.org/2000/svg"
-								width="20"
-								height="20"
-								viewBox="0 0 24 24"
-							>
-								<path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-							</svg>
-						</a>
-						<ul className="p-2">
-							<li>
-								<a>Submenu 1</a>
-							</li>
-							<li>
-								<a>Submenu 2</a>
-							</li>
-						</ul>
+						<Link href="/about-us">About Us</Link>
 					</li>
 					<li>
-						<a>Item 3</a>
+						<Link href="/faq">FAQ</Link>
 					</li>
 				</ul>
 			</div>
 			<div className="navbar-end">
-				<a className="btn">Get started</a>
+				<Link
+					className="btn"
+					href="/login"
+				>
+					Join Us
+				</Link>
+				{/* <a className="btn">Get started</a> */}
 			</div>
 		</div>
 	);
