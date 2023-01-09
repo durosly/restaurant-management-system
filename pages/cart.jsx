3d7d9e0fbcd451d993bf8b2f4b1abc3ea4149a2c
durosly <slyboydon1@gmail.com>
2023-01-09 01:00:09 +0100

    update cart display

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -6,9 +6,9 @@ function Cart() {
 		<UserWrapper>
 			<h2 className="text-6xl text-center my-5">Cart</h2>
 			<div className="container">
-				<ul className="list-none my-5 space-y-4">
+				<ul className="list-none my-5 space-y-4 max-w-lg mx-auto">
 					<li>
-						<div className="card lg:card-side bg-info-content shadow-xl max-w-lg mx-auto px-4">
+						<div className="card lg:card-side bg-info-content shadow-xl  px-4">
 							<figure>
 								<img
 									className="w-[100px] h-[100px]"
@@ -17,7 +17,10 @@ function Cart() {
 								/>
 							</figure>
 							<div className="card-body">
-								<h2 className="card-title">Fried rice</h2>
+								<h2 className="card-title">
+									Fried rice -{" "}
+									<span className="font-bold">N7,000</span>
+								</h2>
 								<p>
 									Click the button to listen on Spotiwhy app.
 								</p>
@@ -65,8 +68,25 @@ function Cart() {
 						</div>
 					</li>
 				</ul>
-				<div className="text-center mb-5">
-					<button className="btn btn-primary">Checkout</button>
+				<div className="max-w-lg mx-auto">
+					<div className="divider"></div>
+					<div className="flex justify-between">
+						<p className="text-xl">Sub total:</p>
+						<p className="text-xl font-bold">80000</p>
+					</div>
+				</div>
+				<div className="text-center my-5">
+					<button className="btn btn-primary gap-2">
+						<svg
+							className="w-5 h-5 fill-current"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 512 512"
+						>
+							{/*! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}
+							<path d="M64 0C46.3 0 32 14.3 32 32V96c0 17.7 14.3 32 32 32h80v32H87c-31.6 0-58.5 23.1-63.3 54.4L1.1 364.1C.4 368.8 0 373.6 0 378.4V448c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V378.4c0-4.8-.4-9.6-1.1-14.4L488.2 214.4C483.5 183.1 456.6 160 425 160H208V128h80c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H64zM96 48H256c8.8 0 16 7.2 16 16s-7.2 16-16 16H96c-8.8 0-16-7.2-16-16s7.2-16 16-16zM64 432c0-8.8 7.2-16 16-16H432c8.8 0 16 7.2 16 16s-7.2 16-16 16H80c-8.8 0-16-7.2-16-16zm48-216c13.3 0 24 10.7 24 24s-10.7 24-24 24s-24-10.7-24-24s10.7-24 24-24zm72 24c0-13.3 10.7-24 24-24s24 10.7 24 24s-10.7 24-24 24s-24-10.7-24-24zm-24 56c13.3 0 24 10.7 24 24s-10.7 24-24 24s-24-10.7-24-24s10.7-24 24-24zm120-56c0-13.3 10.7-24 24-24s24 10.7 24 24s-10.7 24-24 24s-24-10.7-24-24zm-24 56c13.3 0 24 10.7 24 24s-10.7 24-24 24s-24-10.7-24-24s10.7-24 24-24zm120-56c0-13.3 10.7-24 24-24s24 10.7 24 24s-10.7 24-24 24s-24-10.7-24-24zm-24 56c13.3 0 24 10.7 24 24s-10.7 24-24 24s-24-10.7-24-24s10.7-24 24-24z" />
+						</svg>
+						Checkout
+					</button>
 				</div>
 			</div>
 		</UserWrapper>
