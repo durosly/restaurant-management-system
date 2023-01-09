ceb2351bc000fdcd28a3ff491693e107a0226638
durosly <slyboydon1@gmail.com>
2023-01-09 03:38:51 +0100

    create admin layout

---
+++ pages/admin/index.jsx
@@ -0,0 +1,40 @@
+function Login() {
+	return (
+		<div className="hero min-h-screen bg-base-200">
+			<div className="hero-content flex-col">
+				<div className="text-center">
+					<h1 className="text-5xl font-bold">Staff Login</h1>
+				</div>
+				<div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+					<div className="card-body">
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Email</span>
+							</label>
+							<input
+								type="text"
+								placeholder="email"
+								className="input input-bordered"
+							/>
+						</div>
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Password</span>
+							</label>
+							<input
+								type="text"
+								placeholder="password"
+								className="input input-bordered"
+							/>
+						</div>
+						<div className="form-control mt-6">
+							<button className="btn btn-primary">Login</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Login;
